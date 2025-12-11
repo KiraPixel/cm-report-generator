05b07ea081b883b728bfe5b8773d50aaf647c60a
Kira <thepowerkira@gmail.com>
2025-12-11 10:33:49 +0300

    add axenta in main_summary report

--- app/reports/ReportQuering/main_summary.py
+++ app/reports/ReportQuering/main_summary.py
@@ -1,31 +1,29 @@
 from sqlalchemy import func
 
-from app.models import Transport, Storage, TransportModel, CashWialon, CashCesar
+from app.models import Transport, Storage, TransportModel, CashWialon, CashCesar, CashAxenta
 from app.reports import ReportObject
 
 
 class MainSummary(ReportObject):
     name = "main_summary"
-    headers = ['Тип', 'Регион', 'Склад', '№ Лота', 'Модель', 'Тип подъемника', 'Тип двигателя', 'parser_1c','Cesar Position', 'Wialon']
+    headers = ['Тип', 'Регион', 'Склад', '№ Лота', 'Модель', 'Тип подъемника', 'Тип двигателя', 'parser_1c','Cesar Position', 'Wialon', 'Axenta']
 
     def processing(self):
         query = self.db_session.query(Transport, Storage, TransportModel,
-            self.db_session.query(func.count()).filter(
-                CashCesar.object_name.like(func.concat('%', Transport.uNumber, '%'))
-            ).label("cesar_count"),
-            self.db_session.query(func.count()).filter(
-                CashWialon.nm.like(func.concat('%', Transport.uNumber, '%'))
-            ).label("wialon_count")
-                ).join(TransportModel, Transport.model_id == TransportModel.id, isouter=True
-                ).join(Storage, Transport.storage_id == Storage.ID, isouter=True).all()
+                                      self.db_session.query(func.count()).filter(
+                                          CashCesar.object_name.like(func.concat('%', Transport.uNumber, '%'))
+                                      ).scalar_subquery().label("cesar_count"),
+                                      self.db_session.query(func.count()).filter(
+                                          CashWialon.nm.like(func.concat('%', Transport.uNumber, '%'))
+                                      ).scalar_subquery().label("wialon_count"),
+                                      self.db_session.query(func.count()).filter(
+                                          CashAxenta.nm.like(func.concat('%', Transport.uNumber, '%'))
+                                      ).scalar_subquery().label("axenta_count")
+                                      ).join(TransportModel, Transport.model_id == TransportModel.id, isouter=True
+                                             ).join(Storage, Transport.storage_id == Storage.ID, isouter=True).all()
 
         for row in query:
-            transport: Transport
-            storage: Storage
-            transport_model: TransportModel
-            cesar_count: int
-            wialon_count: int
-            transport, storage, transport_model, cesar_count, wialon_count = row
+            transport, storage, transport_model, cesar_count, wialon_count, axenta_count = row
 
             self.values.append([transport_model.type or '',
                                 storage.region or '',
@@ -36,5 +34,6 @@ class MainSummary(ReportObject):
                                 transport_model.engine or '',
                                 transport.parser_1c or 0,
                                 cesar_count or 0,
-                                wialon_count or 0
+                                wialon_count or 0,
+                                axenta_count or 0
                                 ])
